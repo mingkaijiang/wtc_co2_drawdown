eaba7ab701366a5efc17a1258871e34ea71f8d02
mingkaijiang <shanyujordan512@gmail.com>
2019-01-23 11:32:37 +1100

    some preliminary results drawn

---
+++ prepare.R
@@ -0,0 +1,24 @@
+
+#### Create output folder
+if(!dir.exists("output")) {
+    dir.create("output", showWarnings = FALSE)
+}
+
+#### Install packages
+if(!require(pacman))install.packages("pacman")
+pacman::p_load(dplyr, 
+               doBy, 
+               lubridate,
+               ggplot2,
+               viridis,
+               sciplot,
+               scales,
+               data.table)    
+
+
+#### Sourcing all R files in the modules subdirectory
+sourcefiles <- dir("scripts", pattern="[.]R$", recursive = TRUE, full.names = TRUE)
+for(z in sourcefiles)source(z)
+
+
+
---
+++ run_program.R
@@ -0,0 +1,102 @@
+#### This script is used to process WCT1 CO2 drawdown experiment data
+###
+### author: Mingkai Jiang
+###         m.jiang@westernsydney.edu.au
+###
+
+#### read in necessary stuffs
+source("prepare.R")
+
+#### read in raw data
+myDF <- read.table("data/mergeall.txt", sep=",", header=T)
+
+### set up dataset
+myDF$canopy <- as.character(myDF$canopy)
+myDF$datetime <- as.POSIXct(as.character(myDF$datetime))
+
+### extract time information
+myDF$time <- strftime(myDF$datetime, format="%H:%M:%S")
+myDF$time <- as.POSIXct(myDF$time, format="%H:%M:%S")
+
+
+### data explained
+### column canopy: 12345 - full canopy
+###                345 - middle canopy
+###                45 - lower canopy
+###                0 - no canopy
+
+### need to correct for different sizes of trees
+
+### plot overall data
+p1 <- ggplot(myDF, aes(datetime))+
+    geom_point(aes(y=CO2Local, shape=factor(canopy), color=factor(chamber)), size=2.0)+
+    labs(x="Date Time", y=expression(paste(CO[2], " concentration (ppm)")))+
+    theme_linedraw() +
+    theme(panel.grid.minor=element_blank(),
+          axis.title.x = element_text(size=14), 
+          axis.text.x = element_text(size=12),
+          axis.text.y=element_text(size=12),
+          axis.title.y=element_text(size=14),
+          legend.text=element_text(size=12),
+          legend.title=element_text(size=14),
+          panel.grid.major=element_blank(),
+          legend.position="right")+
+    scale_colour_manual(name="Chamber", values = c("1" = "brown", "2" = "red", "3" = "pink",
+                                                   "4" = "orange", "7" = "yellow", "8" = "green",
+                                                   "11" = "cyan", "12" = "blue"))+
+    scale_shape_manual(name="Canopy", values = c("12345" = 16, "345" = 17, "45" = 15),
+                       labels=c("full", "top + middle", "top"))+
+    scale_x_datetime(labels = date_format("%b %d %H:%M:%S"))
+
+
+p2 <- ggplot(myDF, aes(SumOfarea_fully_exp))+
+    geom_point(aes(y=CO2Local, shape=factor(canopy), color=factor(chamber)), size=2.0)+
+    labs(x="Sum of leaf area", y=expression(paste(CO[2], " concentration (ppm)")))+
+    theme_linedraw() +
+    theme(panel.grid.minor=element_blank(),
+          axis.title.x = element_text(size=14), 
+          axis.text.x = element_text(size=12),
+          axis.text.y=element_text(size=12),
+          axis.title.y=element_text(size=14),
+          legend.text=element_text(size=12),
+          legend.title=element_text(size=14),
+          panel.grid.major=element_blank(),
+          legend.position="right")+
+    scale_colour_manual(name="Chamber", values = c("1" = "brown", "2" = "red", "3" = "pink",
+                                                   "4" = "orange", "7" = "yellow", "8" = "green",
+                                                   "11" = "cyan", "12" = "blue"))+
+    scale_shape_manual(name="Canopy", values = c("12345" = 16, "345" = 17, "45" = 15),
+                       labels=c("full", "top + middle", "top"))
+
+### plot all responses onto the same day
+p3 <- ggplot(myDF, aes(time))+
+    geom_point(aes(y=CO2Local, shape=factor(canopy), color=factor(chamber)), size=2.0)+
+    labs(x="Time", y=expression(paste(CO[2], " concentration (ppm)")))+
+    theme_linedraw() +
+    theme(panel.grid.minor=element_blank(),
+          axis.title.x = element_text(size=14), 
+          axis.text.x = element_text(size=12),
+          axis.text.y=element_text(size=12),
+          axis.title.y=element_text(size=14),
+          legend.text=element_text(size=12),
+          legend.title=element_text(size=14),
+          panel.grid.major=element_blank(),
+          legend.position="right")+
+    scale_colour_manual(name="Chamber", values = c("1" = "brown", "2" = "red", "3" = "pink",
+                                                   "4" = "orange", "7" = "yellow", "8" = "green",
+                                                   "11" = "cyan", "12" = "blue"))+
+    scale_shape_manual(name="Canopy", values = c("12345" = 16, "345" = 17, "45" = 15),
+                       labels=c("full", "top + middle", "top"))+
+    scale_x_datetime(limits = as.POSIXct(c('2019-01-23 07:00:00','2019-01-23 16:59:59')))
+
+### output
+pdf("output/overall_data.pdf", width=12, height=6)
+plot(p1)
+plot(p2)
+plot(p3)
+dev.off()
+
+
+
+    
+
