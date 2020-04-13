b8b3d6b9e333c1a31eb14c1d6ad38462f1bafa98
mingkaijiang <shanyujordan512@gmail.com>
2020-04-13 12:00:09 +1000

    canopy df possibly ok. next to add Ci and then run fitACi

--- scripts/canopy/canopy_data_per_second_check_and_plot.R
+++ scripts/canopy/canopy_data_per_second_check_and_plot.R
@@ -1018,4 +1018,189 @@ canopy_data_per_second_check_and_plot <- function(inDF){
     plot(p9)
     plot(p10)
     dev.off()
+    
+    
+    
+    ### Chamber 1 example
+    p3 <- ggplot(inDF[inDF$Chamber==1,], aes(x=WTC_CO2, y=Norm_H2O_flux, color=factor(Canopy)))+
+        geom_point(size=2.0)+
+        geom_smooth(se=TRUE, method="gam", formula = y~s(x))+
+        labs(x=expression(CO[2], " (ppm)"), 
+             y=expression(paste(H[2], "O flux (umol ", H[2], "O ", min^-1, ")")))+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.title.x = element_text(size=14), 
+              axis.text.x = element_text(size=12),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="right")+
+        scale_colour_manual(name="Canopy", values = c("12345" = "red", "345" = "green",
+                                                      "45" = "blue"),
+                            labels=c("full", "top + middle", "top"))+
+        ggtitle("Chamber 1 example")
+    
+    ### Chamber 2 example
+    p4 <- ggplot(inDF[inDF$Chamber==2,], aes(x=WTC_CO2, y=Norm_H2O_flux, color=factor(Canopy)))+
+        geom_point(size=2.0)+
+        geom_smooth(se=TRUE, method="gam", formula = y~s(x))+
+        labs(x=expression(CO[2], " (ppm)"), 
+             y=expression(paste(H[2], "O flux (umol ", H[2], "O ", min^-1, ")")))+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.title.x = element_text(size=14), 
+              axis.text.x = element_text(size=12),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="right")+
+        scale_colour_manual(name="Canopy", values = c("12345" = "red", "345" = "green",
+                                                      "45" = "blue"),
+                            labels=c("full", "top + middle", "top"))+
+        ggtitle("Chamber 2 example")
+    
+    ### Chamber 3 example
+    p5 <- ggplot(inDF[inDF$Chamber==3,], aes(x=WTC_CO2, y=Norm_H2O_flux, color=factor(Canopy)))+
+        geom_point(size=2.0)+
+        geom_smooth(se=TRUE, method="gam", formula = y~s(x))+
+        labs(x=expression(CO[2], " (ppm)"), 
+             y=expression(paste(H[2], "O flux (umol ", H[2], "O ", min^-1, ")")))+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.title.x = element_text(size=14), 
+              axis.text.x = element_text(size=12),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="right")+
+        scale_colour_manual(name="Canopy", values = c("12345" = "red", "345" = "green",
+                                                      "45" = "blue"),
+                            labels=c("full", "top + middle", "top"))+
+        ggtitle("Chamber 3 example")
+    
+    
+    ### Chamber 4 example
+    p6 <- ggplot(inDF[inDF$Chamber==4,], aes(x=WTC_CO2, y=Norm_H2O_flux, color=factor(Canopy)))+
+        geom_point(size=2.0)+
+        geom_smooth(se=TRUE, method="gam", formula = y~s(x))+
+        labs(x=expression(CO[2], " (ppm)"), 
+             y=expression(paste(H[2], "O flux (umol ", H[2], "O ", min^-1, ")")))+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.title.x = element_text(size=14), 
+              axis.text.x = element_text(size=12),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="right")+
+        scale_colour_manual(name="Canopy", values = c("12345" = "red", "345" = "green",
+                                                      "45" = "blue"),
+                            labels=c("full", "top + middle", "top"))+
+        ggtitle("Chamber 4 example")
+    
+    ### Chamber 7 example
+    p7 <- ggplot(inDF[inDF$Chamber==7,], aes(x=WTC_CO2, y=Norm_H2O_flux, color=factor(Canopy)))+
+        geom_point(size=2.0)+
+        geom_smooth(se=TRUE, method="gam", formula = y~s(x))+
+        labs(x=expression(CO[2], " (ppm)"), 
+             y=expression(paste(H[2], "O flux (umol ", H[2], "O ", min^-1, ")")))+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.title.x = element_text(size=14), 
+              axis.text.x = element_text(size=12),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="right")+
+        scale_colour_manual(name="Canopy", values = c("12345" = "red", "345" = "green",
+                                                      "45" = "blue"),
+                            labels=c("full", "top + middle", "top"))+
+        ggtitle("Chamber 7 example")
+    
+    ### Chamber 8 example
+    p8 <- ggplot(inDF[inDF$Chamber==8,], aes(x=WTC_CO2, y=Norm_H2O_flux, color=factor(Canopy)))+
+        geom_point(size=2.0)+
+        geom_smooth(se=TRUE, method="gam", formula = y~s(x))+
+        labs(x=expression(CO[2], " (ppm)"), 
+             y=expression(paste(H[2], "O flux (umol ", H[2], "O ", min^-1, ")")))+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.title.x = element_text(size=14), 
+              axis.text.x = element_text(size=12),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="right")+
+        scale_colour_manual(name="Canopy", values = c("12345" = "red", "345" = "green",
+                                                      "45" = "blue"),
+                            labels=c("full", "top + middle", "top"))+
+        ggtitle("Chamber 8 example")
+    
+    ### Chamber 11 example
+    p9 <- ggplot(inDF[inDF$Chamber==11,], aes(x=WTC_CO2, y=Norm_H2O_flux, color=factor(Canopy)))+
+        geom_point(size=2.0)+
+        geom_smooth(se=TRUE, method="gam", formula = y~s(x))+
+        labs(x=expression(CO[2], " (ppm)"), 
+             y=expression(paste(H[2], "O flux (umol ", H[2], "O ", min^-1, ")")))+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.title.x = element_text(size=14), 
+              axis.text.x = element_text(size=12),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="right")+
+        scale_colour_manual(name="Canopy", values = c("12345" = "red", "345" = "green",
+                                                      "45" = "blue"),
+                            labels=c("full", "top + middle", "top"))+
+        ggtitle("Chamber 11 example")
+    
+    ### Chamber 12 example
+    p10 <- ggplot(inDF[inDF$Chamber==12,], aes(x=WTC_CO2, y=Norm_H2O_flux, color=factor(Canopy)))+
+        geom_point(size=2.0)+
+        geom_smooth(se=TRUE, method="gam", formula = y~s(x))+
+        labs(x=expression(CO[2], " (ppm)"), 
+             y=expression(paste(H[2], "O flux (umol ", H[2], "O ", min^-1, ")")))+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.title.x = element_text(size=14), 
+              axis.text.x = element_text(size=12),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="right")+
+        scale_colour_manual(name="Canopy", values = c("12345" = "red", "345" = "green",
+                                                      "45" = "blue"),
+                            labels=c("full", "top + middle", "top"))+
+        ggtitle("Chamber 12 example")
+    
+    
+    ### output
+    pdf("output/canopy_data_chamber_H2O_drawdown_fluxes_over_Ca.pdf", width=12, height=6)
+    
+    plot(p3)
+    plot(p4)
+    plot(p5)
+    plot(p6)
+    plot(p7)
+    plot(p8)
+    plot(p9)
+    plot(p10)
+    dev.off()
 }
