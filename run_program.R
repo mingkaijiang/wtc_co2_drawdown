bc943aad60b7bf63fd43382f9df95212c98b3200
mingkaijiang <shanyujordan512@gmail.com>
2020-04-16 13:29:05 +1000

    more cleaning

--- run_program.R
+++ run_program.R
@@ -71,25 +71,12 @@ plot_A_Ca_for_leaf_and_canopy_data(cDF=canopyDF)
 ### including statistics
 plot_A_Ca_for_leaf_and_canopy_data_ambient_treatment(cDF=canopyDF)
 
-
-####### Statistics leaf vs. canopy
-
-
-####### Statistics CO2 and water treatment
+#summary_statistics_A_Ci_curves()
 
 
-####### Plot key parameter summary figure & add stats
+############################# Roger 2017 results ######################
+###
 
-
-
-
-
-#### compare canopy scaled down data with leaf data 
-compare_ACI_results_with_canopy_and_leaf_data(cDF=canopyDF)
-
-
-
-summary_statistics_A_Ci_curves()
 
 
 
@@ -97,10 +84,10 @@ summary_statistics_A_Ci_curves()
 ### this script generate met data based on 2009 met data collected at WTC
 ### need to combine with canopy drawdown data from inside the chambers
 ### this resulted in half hourly met data, which is probably too coarse
-generate_met_data_2009()
+#generate_met_data_2009()
 
 ### second method: to linearly interpolate hourly met data onto per minute basis, then run the simulation
-generate_met_data_2009_method_2()
+#generate_met_data_2009_method_2()
 
 #### this script generate met data for period 2008.04.14 to 2009.03.06
 #### which is before the co2 drawdown experiment
@@ -109,9 +96,7 @@ generate_met_data_2009_method_2()
 
 
 ############## compare modeled (leaf scaled-up) with canopy (CO2 drawdown) fluxes ##############
-compare_chamber_results_at_canopy_level()
-
-
+#compare_chamber_results_at_canopy_level()
 
 
 ########## End ############
@@ -119,26 +104,4 @@ compare_chamber_results_at_canopy_level()
 ### to do:
 ### 1. generate met data at per minute interval and re-run the models
 ### 2. compare predicted canopy response at this time interval
-### 3. resolve the issue of canopy E: what unit in the predicted E in the model?
-###                                   what unit in the data?
-###                                   why such a large contrast?
-
-### to decide:
-### 1. what parameters to use to drive the model? Is it based on all data (i.e. 2008-2009) for each chamber?
-### 2. how to parameterize the two-leaf model? One using up canopy parameters, or use the averaged parameter?
-### 3. are the parameters currently used logical? Check, especially Rd. 
-### 
-
-
-### to do: compare WTC leaf A-CI with EucFACE A-CI to check the CO2 responsiveness (WTC only 10%, not right)
-### Plot Ci = 280, not Ca = 400 on individual A-CI plots
-### a subplot for the region of 400-600 ppm, to see the slope of the curve
-### Rd, take the average of all.
-### don't use Km. 
-### for leaf-scale A-CI, fit all dates, and average for each chamber and position. 
-
-### there is a unit issue in calculating water fluxes
-### which then affected stomatal conductance calculation, and eventually Ci calculation
-### will need to resolve this to get reasonable estimate of Ci. 
-
 
