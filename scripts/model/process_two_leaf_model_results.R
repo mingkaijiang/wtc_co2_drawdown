d3531a2f221f36435f273cf80a9e191b56b37b0c
mingkaijiang <shanyujordan512@gmail.com>
2020-11-16 11:42:19 +1100

    two leaf results plotted

--- scripts/model/process_two_leaf_model_results.R
+++ scripts/model/process_two_leaf_model_results.R
@@ -166,53 +166,7 @@ process_two_leaf_model_results <- function() {
     ### save output
     write.csv(outDF, "output/simulated/two_leaf_result_summary.csv", row.names=F)
     
-    ### plot two leaf results
-    p1 <- ggplot(data=outDF, 
-                 aes(CO2_treatment, Acan, group=canopy)) +
-        #geom_rect(aes(xmin = 0, xmax = 2.5, 
-        #              ymin = 0.8, ymax = 1.6),
-        #          alpha = 0.2, fill = "lightgray")+
-        geom_bar(stat = "identity", aes(fill=canopy), 
-                 position="dodge") +
-        #geom_vline(xintercept=2.5, lty=2)+
-        #geom_vline(xintercept=3.5, lty=2)+
-        theme_linedraw() +
-        theme(panel.grid.minor=element_blank(),
-              axis.text.x=element_text(size=10),
-              axis.title.x=element_text(size=14),
-              axis.text.y=element_text(size=12),
-              axis.title.y=element_text(size=14),
-              legend.text=element_text(size=12),
-              legend.title=element_text(size=14),
-              panel.grid.major=element_blank(),
-              legend.position="right",
-              legend.box = 'vertical',
-              legend.box.just = 'right',
-              plot.title = element_text(size=16, face="bold", 
-                                        hjust = 0.5),
-              legend.text.align = 0)+
-        ylab(expression(paste(Delta * A * " / " * A[400])))+
-        #scale_x_discrete(breaks=c("1_up", "2_low", "3_Full", "4_MATE", "5_MATE", 
-        #                          "6_two_leaf", "7_multi"),
-        #                 labels=c("Up", 
-        #                          "Low",
-        #                          "Full",
-        #                          "MATE",
-        #                          "MATE2",
-        #                          "Two-leaf",
-        #                          "Multi-layer"))+
-        #scale_fill_manual(name="",
-        #                  breaks=c("A", "Ac", "Aj", "A1sun", "A2sha"),
-        #                  labels=c("A", expression(paste(A[c])),
-        #                           expression(paste(A[j])),
-        #                           expression(paste(A[sun])),
-        #                           expression(paste(A[sha]))),
-        #                  values = colorblind_pal()(5 + 1)[-1])+
-        xlab("")+
-        coord_cartesian(ylim = c(0.18, 0.22)) 
-    
-    
-    plot(p1)
+
   
     ### plot APAR and LAI by sun and shaded leaf layers
     plotDF2 <- data.frame(rep(c("12345", "345", "45"), each=2),
@@ -243,7 +197,7 @@ process_two_leaf_model_results <- function() {
     plotDF2$lab <- gsub("45", "3_T", plotDF2$lab)
     
     ## plot
-    p2 <- ggplot(data=plotDF2, 
+    p1 <- ggplot(data=plotDF2, 
                  aes(lab, APAR, group=layer)) +
         geom_bar(stat = "identity", aes(fill=layer), 
                  position="stack") +
@@ -256,7 +210,7 @@ process_two_leaf_model_results <- function() {
               legend.text=element_text(size=12),
               legend.title=element_text(size=14),
               panel.grid.major=element_blank(),
-              legend.position="right",
+              legend.position="none",
               legend.box = 'vertical',
               legend.box.just = 'right',
               plot.title = element_text(size=16, face="bold", 
@@ -267,14 +221,14 @@ process_two_leaf_model_results <- function() {
                          labels=c("Full",
                                   "T+M",
                                   "T"))+
-        scale_fill_manual(name="",
+        scale_fill_manual(name="Leaf layer",
                           breaks=c("sun", "shade"),
                           labels=c("sun", "shade"),
                           values = colorblind_pal()(2 + 1)[-1])+
     xlab("")
         
     
-    p3 <- ggplot(data=plotDF2, 
+    p2 <- ggplot(data=plotDF2, 
                  aes(lab, LAI, group=layer)) +
         geom_bar(stat = "identity", aes(fill=layer), 
                  position="stack") +
@@ -287,7 +241,7 @@ process_two_leaf_model_results <- function() {
               legend.text=element_text(size=12),
               legend.title=element_text(size=14),
               panel.grid.major=element_blank(),
-              legend.position="right",
+              legend.position="none",
               legend.box = 'vertical',
               legend.box.just = 'right',
               plot.title = element_text(size=16, face="bold", 
@@ -298,13 +252,150 @@ process_two_leaf_model_results <- function() {
                          labels=c("Full",
                                   "T+M",
                                   "T"))+
-        scale_fill_manual(name="",
+        scale_fill_manual(name="leaf layer",
                           breaks=c("sun", "shade"),
                           labels=c("sun", "shade"),
                           values = colorblind_pal()(2 + 1)[-1])+
         xlab("")
     
-    plot(p3)
+    
+    ## pdf
+    legend_shared <- get_legend(p1 + theme(legend.position="bottom",
+                                           legend.box = 'vertical',
+                                           legend.box.just = 'left'))
+    
+    combined_plots <- plot_grid(p1, p2, 
+                                labels=c("(a)", "(b)"),
+                                ncol=1, align="vh", axis = "l",
+                                label_x=0.8, label_y=0.95,
+                                label_size = 18)
+    
+    
+    pdf("output/simulated/two_leaf_result_APAR_LAI.pdf", width=4, height=6)
+    plot_grid(combined_plots, legend_shared, ncol=1, rel_heights=c(1,0.1))
+    dev.off()  
+    
+    
+    
+    ### plot sensitivity
+    plotDF1 <- data.frame(rep(c("12345", "345", "45"), each=3),
+                          rep(c("Anet", "Asun", "Ashade"), 3),
+                          NA)
+    colnames(plotDF1) <- c("canopy", "layer", "A")
+    plotDF1$lab <- plotDF1$canopy
+    plotDF1$lab <- gsub("12345", "1_Full", plotDF1$lab)
+    plotDF1$lab <- gsub("345", "2_TM", plotDF1$lab)
+    plotDF1$lab <- gsub("45", "3_T", plotDF1$lab)
+    
+    plotDF2 <- plotDF1
+    
+    
+    ## assign values
+    plotDF1$A[plotDF1$canopy=="12345"&plotDF1$layer=="Anet"] <- outDF$Acan[outDF$CO2_treatment=="aCO2"&outDF$canopy=="12345"]
+    plotDF1$A[plotDF1$canopy=="12345"&plotDF1$layer=="Asun"] <- outDF$Asun[outDF$CO2_treatment=="aCO2"&outDF$canopy=="12345"]
+    plotDF1$A[plotDF1$canopy=="12345"&plotDF1$layer=="Ashade"] <- outDF$Asha[outDF$CO2_treatment=="aCO2"&outDF$canopy=="12345"]
+    
+    plotDF1$A[plotDF1$canopy=="345"&plotDF1$layer=="Anet"] <- outDF$Acan[outDF$CO2_treatment=="aCO2"&outDF$canopy=="345"]
+    plotDF1$A[plotDF1$canopy=="345"&plotDF1$layer=="Asun"] <- outDF$Asun[outDF$CO2_treatment=="aCO2"&outDF$canopy=="345"]
+    plotDF1$A[plotDF1$canopy=="345"&plotDF1$layer=="Ashade"] <- outDF$Asha[outDF$CO2_treatment=="aCO2"&outDF$canopy=="345"]
+    
+    plotDF1$A[plotDF1$canopy=="45"&plotDF1$layer=="Anet"] <- outDF$Acan[outDF$CO2_treatment=="aCO2"&outDF$canopy=="45"]
+    plotDF1$A[plotDF1$canopy=="45"&plotDF1$layer=="Asun"] <- outDF$Asun[outDF$CO2_treatment=="aCO2"&outDF$canopy=="45"]
+    plotDF1$A[plotDF1$canopy=="45"&plotDF1$layer=="Ashade"] <- outDF$Asha[outDF$CO2_treatment=="aCO2"&outDF$canopy=="45"]
+    
+    
+    plotDF2$A[plotDF2$canopy=="12345"&plotDF2$layer=="Anet"] <- outDF$Acan[outDF$CO2_treatment=="eCO2"&outDF$canopy=="12345"]
+    plotDF2$A[plotDF2$canopy=="12345"&plotDF2$layer=="Asun"] <- outDF$Asun[outDF$CO2_treatment=="eCO2"&outDF$canopy=="12345"]
+    plotDF2$A[plotDF2$canopy=="12345"&plotDF2$layer=="Ashade"] <- outDF$Asha[outDF$CO2_treatment=="eCO2"&outDF$canopy=="12345"]
+    
+    plotDF2$A[plotDF2$canopy=="345"&plotDF2$layer=="Anet"] <- outDF$Acan[outDF$CO2_treatment=="eCO2"&outDF$canopy=="345"]
+    plotDF2$A[plotDF2$canopy=="345"&plotDF2$layer=="Asun"] <- outDF$Asun[outDF$CO2_treatment=="eCO2"&outDF$canopy=="345"]
+    plotDF2$A[plotDF2$canopy=="345"&plotDF2$layer=="Ashade"] <- outDF$Asha[outDF$CO2_treatment=="eCO2"&outDF$canopy=="345"]
+    
+    plotDF2$A[plotDF2$canopy=="45"&plotDF2$layer=="Anet"] <- outDF$Acan[outDF$CO2_treatment=="eCO2"&outDF$canopy=="45"]
+    plotDF2$A[plotDF2$canopy=="45"&plotDF2$layer=="Asun"] <- outDF$Asun[outDF$CO2_treatment=="eCO2"&outDF$canopy=="45"]
+    plotDF2$A[plotDF2$canopy=="45"&plotDF2$layer=="Ashade"] <- outDF$Asha[outDF$CO2_treatment=="eCO2"&outDF$canopy=="45"]
+    
+    
+    ### plot two leaf results
+    p1 <- ggplot(data=plotDF1, 
+                 aes(lab, A, group=layer)) +
+        geom_bar(stat = "identity", aes(fill=layer), 
+                 position="dodge") +
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.text.x=element_text(size=10),
+              axis.title.x=element_text(size=14),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="none",
+              legend.box = 'vertical',
+              legend.box.just = 'right',
+              plot.title = element_text(size=16, face="bold", 
+                                        hjust = 0.5),
+              legend.text.align = 0)+
+        ylab(expression(paste(Delta * A * " / " * A[400])))+
+        scale_x_discrete(breaks=c("1_Full", "2_TM", "3_T"),
+                         labels=c("Full",
+                                  "T+M",
+                                  "T"))+
+        scale_fill_manual(name="leaf layer",
+                          breaks=c("Anet", "Asun", "Ashade"),
+                          labels=c("canopy", "sun", "shade"),
+                          values = c("grey", colorblind_pal()(2 + 1)[-1]))+
+    xlab(expression(aCO[2]))+
+    coord_cartesian(ylim = c(0.19, 0.21)) 
+    
+    
+    p2 <- ggplot(data=plotDF2, 
+                 aes(lab, A, group=layer)) +
+        geom_bar(stat = "identity", aes(fill=layer), 
+                 position="dodge") +
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.text.x=element_text(size=10),
+              axis.title.x=element_text(size=14),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="none",
+              legend.box = 'vertical',
+              legend.box.just = 'right',
+              plot.title = element_text(size=16, face="bold", 
+                                        hjust = 0.5),
+              legend.text.align = 0)+
+        ylab(expression(paste(Delta * A * " / " * A[400])))+
+        scale_x_discrete(breaks=c("1_Full", "2_TM", "3_T"),
+                         labels=c("Full",
+                                  "T+M",
+                                  "T"))+
+        scale_fill_manual(name="leaf layer",
+                          breaks=c("Anet", "Asun", "Ashade"),
+                          labels=c("canopy", "sun", "shade"),
+                          values = c("grey", colorblind_pal()(2 + 1)[-1]))+
+        xlab(expression(eCO[2]))+
+        coord_cartesian(ylim = c(0.19, 0.21)) 
+    
+    ## pdf
+    legend_shared <- get_legend(p1 + theme(legend.position="bottom",
+                                           legend.box = 'vertical',
+                                           legend.box.just = 'left'))
+    
+    combined_plots <- plot_grid(p1, p2, 
+                                labels=c("(a)", "(b)"),
+                                ncol=2, align="vh", axis = "l",
+                                label_x=0.8, label_y=0.95,
+                                label_size = 18)
+    
+    
+    pdf("output/simulated/two_leaf_result_A_sensitivity.pdf", width=6, height=3)
+    plot_grid(combined_plots, legend_shared, ncol=1, rel_heights=c(1,0.1))
+    dev.off()  
     
 }
 
